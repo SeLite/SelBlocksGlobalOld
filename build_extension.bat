13a850a7e1465ff2397f6d0a9668ef21e2e1322a
peter.kehl <peter.kehl@gmail.com>
2016-01-27 09:01:01 +1100

    Migrating from GoogleCode to GitHub.

---
+++ build_extension.bat
@@ -0,0 +1,11 @@
+@echo off
+REM Before invoking this script, run 'cd <folder where this script is>'
+
+REM Create folder "xpi", if it doesn't exist already
+mkdir xpi 2>nul
+del xpi\*.xpi 2>nul
+
+cd sel-blocks-fx_xpi
+jar cfM ..\xpi\sel-blocks-global.xpi *
+
+cd ..
