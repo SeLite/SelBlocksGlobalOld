e30195091515f4dfb9d0dbf989f1fb0d700808f5
peter.kehl <peter.kehl@gmail.com>
2016-01-27 08:54:50 +1100

    Migrated setup_proxy.* from GoogleCode to GitHub.

---
+++ setup_proxy.bat
@@ -0,0 +1,32 @@
+@echo off
+REM Following is for expansion of variables at runtime - e.g. !e! instead of %e%
+setlocal EnableDelayedExpansion
+SET script_folder=%~dp0
+cd %script_folder:~0,-1%
+
+REM Based on https://developer.mozilla.org/en/Setting_up_extension_development_environment and http://kb.mozillazine.org/Profile_folder_-_Firefox
+REM This script accepts an optional parameter, which is a name of Firefox profile. Otherwise it uses 'default' profile. Either way, the profile must have been created by Firefox (i.e. its folder name must be in standard format).
+if not "%1"=="" (
+    set profile=%1
+) else (
+    set profile=default
+)
+
+FOR /D %%c in ("%APPDATA%\Mozilla\Firefox\Profiles\*.%profile%") DO set p=%%c
+if defined p (
+    set e="%p%\extensions"
+    REM If you have not got any extensions in Firefox profile yet, there is no 'extensions' folder. So create it.
+    if not exist "!e!" (
+      mkdir "!e!"
+    )
+    
+    REM Now set up the actual extensions, unless they are already installed from XPI    
+    REM Do not use: echo %CD%.>target-file. Use: cd >target-file. For some reason %CD% doesn't get updated after I change directory.
+    if not exist "!e!\selblocks-global@selite.googlecode.com.xpi" (
+      cd sel-blocks-fx_xpi
+      cd > "!e!\selblocks-global@selite.googlecode.com"
+      cd ..
+    )
+) else (
+   echo Could not find a Firefox profile "%profile%"
+)
---
+++ setup_proxy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#change dir to where this script is run from:
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+# Based on https://developer.mozilla.org/en/Setting_up_extension_development_environment and http://kb.mozillazine.org/Profile_folder_-_Firefox
+# This script accepts an optional parameter, which is a name of Firefox profile. Otherwise it uses 'default' profile. Either way, the profile must have been created by Firefox (i.e. its folder name must be in standard format).
+if [[ "$1" ]]
+then
+    PROFILE="$1"
+else
+    PROFILE=default
+fi
+HOME_FOLDER=~
+
+cd sel-blocks-fx_xpi
+
+if [ "$(uname)" == "Darwin" ]; then
+   # According to http://kb.mozillazine.org/Profile_folder_-_Firefox  there are two places for Firefox profile folder on Mac OS:
+   # ~/Library/Mozilla/Firefox/Profiles/<profile folder> or ~/Library/Application Support/Firefox/Profiles/<profile folder>
+   # But on Mac OS 10.5.8 and 10.9.1 I could see the second folder only. If you can test both, please update this/send this to me.
+   EXTENSION_FOLDER="$( echo "$HOME_FOLDER/Library/Application Support/Firefox/Profiles"/*.$PROFILE )"
+else
+   EXTENSION_FOLDER="$( echo "$HOME_FOLDER/.mozilla/firefox/"*.$PROFILE )"
+fi
+EXTENSION_FOLDER=$EXTENSION_FOLDER/extensions
+
+# If you haven't got any extensions in Firefox profile yet, there is no 'extensions' folder. So create it.
+if [ ! -e "$EXTENSION_FOLDER" ]
+then
+  mkdir "$EXTENSION_FOLDER"
+fi
+
+if [ ! -e "$EXTENSION_FOLDER"/selblocks-global\@selite.googlecode.com.xpi ]
+then
+  pwd > "$EXTENSION_FOLDER"/selblocks-global@selite.googlecode.com
+fi
+
